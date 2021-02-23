43a493429cadb3c9a5a82715ecd9e1c68a4cc5cb
Dean Wright <dmwspace@yahoo.com>
2021-02-23 00:39:48 -0800

    Started work on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 
 function Header() {
+
+    const zipCodeRef = useRef(null)
+
+    const [zipCode, setZipCode] = useState(0)
+
+    function handleChange(e) {
+        const newZip = e.target
+        setZipCode(newZip)
+    }
+
+    function handleClick() {
+        const url = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=process.env.REACT_APP_OPENWEATHERMAPAPI_KEY`
+        fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            const data = res.data
+            console.log(data)
+        })
+        
+    }
     return (
-    <div>
-        <input type="text"></input><button>Submit</button>
-    </div>
+        <div>
+            <input type="number"
+                placeholder="Enter US Zip Code"
+                ref={zipCodeRef}
+                value={zipCode}
+                onChange={handleChange}
+            ></input><button
+                onClick={handleClick}
+            >Submit</button>
+        </div>
     )
 }
 export default Header
