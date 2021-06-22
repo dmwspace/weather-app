bd25d3d1d9ab3753cec7c37ab461adbaabd82e4d
Dean Wright <dmwspace@yahoo.com>
2021-06-21 18:07:06 -0700

    Added second fetch for forecast

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React, {useState, useRef, useEffect} from "react";
 
 function Header() {
-    const zipCodeRef = useRef(null)
+    const inputRef = useRef(null)
 
-    const [fiveDigits, setFiveDigits] = useState(null)
-    const [zipCode, setZipCode] = useState(0)
+    const [cityInput, setcityInput] = useState(null)
     const [clicked, setClicked] = useState(false)
     const [cityName, setCityName] = useState("")
     const [stateName, setStateName] = useState("")
@@ -14,24 +13,22 @@ function Header() {
     const [windSpeed, setWindSpeed] = useState("")
 
     function handleChange(e) {
-        setFiveDigits(e)
-        console.log(fiveDigits)
+        setcityInput(e)
     }
 
     useEffect(() => {
-        zipCodeRef.current.focus()
-        zipCodeRef.current.value = null
-    }, [cityName])
+        inputRef.current.focus()
+        inputRef.current.value = null
+    }, [cityName, stateName])
 
     function handleClick() {
         const apiKey = process.env.REACT_APP_WEATHERBIT_API_KEY
-        const url = `http://api.weatherbit.io/v2.0/current?postal_code=${fiveDigits}&country=US&units=I&key=${apiKey}`
+        const url = `http://api.weatherbit.io/v2.0/current?city=${cityInput}&country=US&units=I&key=${apiKey}`
+        const url2 = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityInput}&days=6&units=I&key=${apiKey}`
         fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data.data)
             setClicked(true)
-            setZipCode(fiveDigits)
             setCityName(data.data[0].city_name)
             setStateName(data.data[0].state_code)
             setDescription(data.data[0].weather.description)
@@ -39,15 +36,23 @@ function Header() {
             setWindSpeed(data.data[0].wind_spd)
             setWindDirection(data.data[0].wind_cdir)
         })
+        fetch(url2, {header: {'mode': 'no-cors'}})
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data.data)
+        })
     }
     const currentWeatherInfo = clicked ?
-        <div>
-            <h1>Current weather conditions for {zipCode} - {cityName}, {stateName}:</h1>
-            <h2>{description}</h2> 
-            <h2>Temp: {temp} degrees</h2>
-            <h2>Wind: {windDirection} {windSpeed} mph </h2>
-        </div>
-        :null
+        !cityName ?
+            <h1>Sorry could not find that, try again.</h1> :
+            <div>
+                <h1>Current weather conditions for {cityName}, {stateName}:</h1>
+                <h2>{description}</h2> 
+                <h2>Temp: {temp} degrees</h2>
+                <h2>Wind: {windDirection} {windSpeed} mph </h2>
+            </div>
+            :null
 
     const handleKeyPress = e => {
         if (e.key === 'Enter' || e.key === 'Return') {
@@ -58,9 +63,9 @@ function Header() {
     return (
         <div>
             <input type="text"
-                placeholder="Enter US Zip Code"
-                ref={zipCodeRef}
-                //value={zipCode}
+                placeholder="Enter US City, and State"
+                ref={inputRef}
+                //value={city}
                 onChange={(e) => handleChange(e.target.value)}
                 onKeyDown={handleKeyPress}
             ></input><button
