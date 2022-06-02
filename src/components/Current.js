e1769a40a31d83bbde9234ba7a575d350b914233
dean <dean@deans-iMac.home>
2022-06-01 20:08:57 -0700

    Changed cors origins

--- src/components/Current.js
+++ src/components/Current.js
@@ -10,13 +10,13 @@ function Current(props) {
             !props.clicked ?
                 null :
             props.clicked && !props.currentFetched ?
-                <h1>Sorry that zip code could not be found. Please try again.</h1> :
+                <h1>Loading</h1> :
                 <Card bg="light" bsPrefix="current-conditions">
                     <Card.Title><h1>{props.cityName}, {props.stateName}:</h1></Card.Title>
                     <Card.Body>
                         <div>
                             <img src={process.env.PUBLIC_URL + "/icons/" + props.currentIcon + ".png"} alt="Problem" />
-                            <span className="currently">Currently {currentTemp}º,</span>
+                            <span className="currently">Currently {currentTemp}º </span>
                             <div id="feels-like">Feels like: {feelsLike}º</div>
                         </div>
                         <h2 className="wind">Wind: {props.windDirection} {props.windSpeed} mph </h2>
